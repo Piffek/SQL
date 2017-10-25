70950d6598b5ed856382d425842989f3ed20de55
Piffek <patrykpiwko123412@gmail.com>
2017-10-25 20:04:03 +0200

    inner join

--- query.sql
+++ query.sql
@@ -3,3 +3,14 @@ FROM table2
 INNER JOIN table1 ON table1.id = table2.user_id
 INNER JOIN shoop ON shoop.id = table2.shoop_id
 WHERE table1.city = "Dzierżoniów"
+
+
+/*if relation to user not exists in table2*/
+
+SELECT table1.id
+FROM table1
+WHERE NOT EXISTS  (
+  SELECT *
+  FROM table2 
+  WHERE table2.user_id = table1.id
+)
