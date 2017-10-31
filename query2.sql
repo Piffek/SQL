cdcd98bcc532ec9832ead4756bb14c85baa8b7b3
Piffek <patrykpiwko123412@gmail.com>
2017-10-31 16:00:22 +0100

    add case

--- query2.sql
+++ query2.sql
@@ -1,10 +1,18 @@
-SELECT SUM(FAKTURY.BRUTTO), table1.name, table1.group, MIASTA.SIZE
+//--ZAD4
+SELECT SUM(FAKTURY.BRUTTO), (SUM(FAKTURY.BRUTTO)-(SUM(FAKTURY.BRUTTO)*CLIENTS_GROUP.rabat/100)) as after_rabat, table1.name, MIASTA.SIZE, CLIENTS_GROUP.symbol, CLIENTS_GROUP.rabat as rabat
 FROM table1
+
 INNER JOIN FAKTURY
 ON FAKTURY.user_id = table1.id
+
 INNER JOIN MIASTA
 ON  MIASTA.id = table1.city_id
-GROUP BY table1.group
+
+INNER JOIN CLIENTS_GROUP
+ON CLIENTS_GROUP.id = table1.group_id
+
+GROUP BY CLIENTS_GROUP.symbol
+
 ORDER BY
   CASE 
     WHEN MIASTA.SIZE = '>500' THEN 2
@@ -12,3 +20,14 @@ ORDER BY
     WHEN MIASTA.SIZE = '>1000' THEN 3
   END
 DESC
+
+
+
+
+//--ZAD5
+
+SELECT count(table1.id) as user_id, FAKTURY.MOUTH, sum(FAKTURY.BRUTTO) as brutto
+FROM table1
+INNER JOIN FAKTURY
+ON FAKTURY.user_id = table1.id
+WHERE brutto > '25000'
--- table2.sql
+++ table2.sql
@@ -10,20 +10,31 @@
   ('2', 'Bielawa', '<234'),
   ('3', 'Wrocław', '>1000');
   
+   CREATE TABLE IF NOT EXISTS `CLIENTS_GROUP` (
+  `id` int(6),
+  `symbol` varchar(20),
+  `rabat` varchar(12),
+  PRIMARY KEY (`id`)
+  );
+  INSERT INTO `CLIENTS_GROUP` (`id`, `symbol`, `rabat`) VALUES
+  ('1', 'HURT', '5.00%'),
+  ('2', 'DETAL', '0.00%'),
+  ('3', 'VIT', '10.00%');
     
   CREATE TABLE IF NOT EXISTS `table1` (
   `id` int(6),
   `name` varchar(100),
   `city_id` int(6),
-  `group` varchar(100) NOT NULL,
+  `group_id` int(6),
   FOREIGN KEY (`city_id`) REFERENCES MIASTA(`id`),
+  FOREIGN KEY (`group_id`) REFERENCES CLIENTS_GROUP(`id`),
   PRIMARY KEY (`id`)
 ) DEFAULT CHARSET=utf8;
-  INSERT INTO `table1` (`id`, `name`, `city_id`, `group`) VALUES
-  ('1', 'patryk', '1', 'DETAL'),
-  ('2', 'adrian', '2', 'VIP'),
-  ('3', 'zosia', '3', 'HURT'),
-  ('4', 'jola', '2', 'HURT');
+  INSERT INTO `table1` (`id`, `name`, `city_id`, `group_id`) VALUES
+  ('1', 'patryk', '1', '2'),
+  ('2', 'adrian', '2', '3'),
+  ('3', 'Aosia', '3', '1'),
+  ('4', 'jola', '2', '1');
   
   CREATE TABLE IF NOT EXISTS `FAKTURY` (
   `id` int(6),
@@ -34,8 +45,9 @@
   PRIMARY KEY (`id`)
   );
   INSERT INTO `FAKTURY` (`id`, `user_id`, `BRUTTO`, `MOUTH`) VALUES
-  ('1', '1', '500', 'WRZESIEŃ'),
-  ('2', '2', '234', 'WRZESIEŃ'),
-  ('3', '4', '234', 'maj'),
-  ('4', '3', '2654', 'WRZESIEŃ');
+  ('1', '1', '5000', 'WRZESIEŃ'),
+  ('2', '2', '27400', 'WRZESIEŃ'),
+  ('3', '4', '2340', 'maj'),
+  ('4', '3', '26540', 'WRZESIEŃ');
+  
  
